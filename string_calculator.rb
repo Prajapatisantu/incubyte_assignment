0ccfa71437c73aaa3d2d8b34c9923d144760887f
prajapati santu <prajapatisantu5765@gmail.com>
2024-06-12 17:46:02 +0530

    Refactor the code for future extend.

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,14 @@ class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.empty?
 
-    string_numbers.split(/#{""}|[\n]/).map(&:to_i).sum
+    string_numbers.split(/#{delimiter(string_numbers)}|[\n]/).map(&:to_i).sum
   end
 
+  private
+
+  def delimiter(numbers)
+    # Default is ','
+    delimiter_line = numbers.lines.first
+    delimiter_line.include?("//") ? delimiter_line[2..-1].strip : ","
+  end
 end
