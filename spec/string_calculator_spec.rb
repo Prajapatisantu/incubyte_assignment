af5173d73d4f6ee4dab9c4c60b47a9cda9e6fdc9
prajapati santu <prajapatisantu5765@gmail.com>
2024-06-12 17:55:44 +0530

    Refactor the test cases and raised the negative error from the add method.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,28 +4,40 @@ require_relative '../string_calculator.rb'
 
 RSpec.describe StringCalculator do
   describe '#add' do
-    it 'returns 0 for an empty string' do
-      expect(subject.add('')).to eq(0)
+    context 'with empty string' do
+      it 'returns 0 for an empty string' do
+        expect(subject.add('')).to eq(0)
+      end
     end
 
-    it 'returns the number for a single number string' do
-      expect(subject.add('1')).to eq(1)
+    context 'with single number' do
+      it 'returns the number for a single number string' do
+        expect(subject.add('1')).to eq(1)
+      end
     end
 
-    it 'returns the sum of two numbers' do
-      expect(subject.add('1,2')).to eq(3)
+    context 'with two numbers' do
+      it 'returns the sum of two numbers' do
+        expect(subject.add('1,2')).to eq(3)
+      end
     end
 
-    it 'supports new lines as delimiters' do
-      expect(subject.add("1\n2,3")).to eq(6)
+    context 'with new lines between numbers' do
+      it 'supports new lines as delimiters' do
+        expect(subject.add("1\n2,3")).to eq(6)
+      end
     end
 
-    it 'supports custom delimiter definition' do
-      expect(subject.add("//;\n1;2")).to eq(3)
+    context 'with custom delimiter' do
+      it 'supports custom delimiter definition' do
+        expect(subject.add("//;\n1;2")).to eq(3)
+      end
     end
 
-    it 'raises an exception' do
-      expect { subject.add("-1,2,-3") }.to raise_error(StandardError, "negative numbers not allowed: -1,-3")
+    context 'with negative numbers' do
+      it 'raises an exception' do
+        expect { subject.add("-1,2,-3") }.to raise_error(StandardError, "negative numbers not allowed: -1,-3")
+      end
     end
 
   end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,9 @@ class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.empty?
 
+    negatives = string_numbers.split(/#{delimiter(string_numbers)}|[\n]/).map(&:to_i).select { |n| n.negative? }
+    raise StandardError, "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
+
     string_numbers.split(/#{delimiter(string_numbers)}|[\n]/).map(&:to_i).sum
   end
 
