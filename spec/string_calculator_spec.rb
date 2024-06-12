7e33cf9bd140908084a6ae9f6c3ae86b1ed88236
prajapati santu <prajapatisantu5765@gmail.com>
2024-06-12 17:01:08 +0530

    Created class and tested first test case and got failure.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,5 +3,11 @@
 require_relative '../string_calculator.rb'
 
 RSpec.describe StringCalculator do
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(subject.add('')).to eq(0)
+    end
+
+  end
 
 end
