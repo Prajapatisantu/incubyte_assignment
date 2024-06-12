c3cf16a5f61b9ecd53cfeca160f0eaa72b62e79e
prajapati santu <prajapatisantu5765@gmail.com>
2024-06-12 17:34:04 +0530

    Added the test cases to support delimeter.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two numbers' do
       expect(subject.add('1,2')).to eq(3)
     end
-  end
 
+    it 'supports new lines as delimiters' do
+      expect(subject.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports custom delimiter definition' do
+      expect(subject.add("//;\n1;2")).to eq(3)
+    end
+
+  end
 end
